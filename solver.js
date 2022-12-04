7305d6c4300c6e78dccfaee29647709d6b1d4dc4
Tapiocaba <lisa.zz.lin@gmail.com>
2022-12-04 01:51:31 -0500

    Solver options and JS
    The JS keeps returning undefined, not sure why.

---
+++ solver.js
@@ -0,0 +1,13 @@
+// remember to merge this w/ main.js at the end
+
+function calculateOdds() {
+    
+    // Get variables
+    let cardOne = document.querySelector('#user-card-1-id').value;
+    let cardTwo = document.querySelector('#user-card-2-id').value;
+    let dealerCard = document.querySelector('#dealer-card-id').value;
+    
+    // change to selected index text
+    alert(cardOne);
+
+}
